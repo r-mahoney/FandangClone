0c4234867ed19298c60a934236531000e4f466f5
Ryan Mahoney <rsmahoney211@gmail.com>
2024-01-02 12:59:07 -0600

    fixing redirect

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,10 @@ import Head from "next/head";
 import { useRouter } from "next/navigation";
 
 export default function Home() {
-  const router = useRouter()
-  router.push('/movies')
+  if (typeof window) {
+    const router = useRouter();
+    router.push("/movies");
+  }
   return (
     <>
       <Head>
